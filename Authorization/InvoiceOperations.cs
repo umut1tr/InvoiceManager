578f2bddd226ffe6e30c4a07040d0180e86d50ad
Umut <umut1tr@hotmail.com>
2022-11-05 11:39:40 +0100

    Added InvoiceOperations / Auth Infrastructure Constants to avoid copy&paste String error margin

--- Authorization/InvoiceOperations.cs
+++ Authorization/InvoiceOperations.cs
@@ -1,9 +1,27 @@
-﻿namespace IdentityApp.Authorization
+﻿using Microsoft.AspNetCore.Authorization.Infrastructure;
+
+namespace IdentityApp.Authorization
 {
     public class InvoiceOperations
     {
 
+        public static OperationAuthorizationRequirement Create =
+            new OperationAuthorizationRequirement { Name = Constants.CreateOperationName };
 
+        public static OperationAuthorizationRequirement Read =
+            new OperationAuthorizationRequirement { Name = Constants.ReadOperationName };
+
+        public static OperationAuthorizationRequirement Update =
+            new OperationAuthorizationRequirement { Name = Constants.UpdateOperationName };
+
+        public static OperationAuthorizationRequirement Delete =
+            new OperationAuthorizationRequirement { Name = Constants.DeleteOperationName };
+
+        public static OperationAuthorizationRequirement Approved =
+            new OperationAuthorizationRequirement { Name = Constants.ApprovedOperationName };
+
+        public static OperationAuthorizationRequirement Rejected =
+            new OperationAuthorizationRequirement { Name = Constants.RejectedOperationName };
 
     }
 
